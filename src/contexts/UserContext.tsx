ca153269535f0e002a0ee5f14a227d573043486e
Marchi8 <luandeoliveirasoares@gmail.com>
2022-08-25 23:06:37 -0300

    typescript-axios-typed

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -7,23 +7,22 @@ import { useNavigate } from "react-router-dom";
 export const UserContext = createContext<IuseAuth>({} as IuseAuth)
 
 
-export interface UserData {
+export interface IUserData {
     email: string;
     password: string;
 }
 
-export interface UserProps {
+export interface IUserProps {
     children: ReactNode;
 }
 
 export interface IuseAuth {
-    user: string;
-    // setUser: string;
-    onSubmit: (data: UserData) => void;
-    createLog: (data: Icadastro) => void;
+    user: IResponseUserData;
+    onSubmit: (data: IUserData) => void;
+    createLog: (data: ICadastro) => void;
 }
 
-export interface Icadastro {
+export interface ICadastro {
     name: string;
     email: string;
     password: string;
@@ -33,16 +32,56 @@ export interface Icadastro {
     course_module: string;
 }
 
-function UserProvider({ children }: UserProps) {
+interface IResponseUser {
+    token: string;
+    user: IResponseUserData;
+    setUser: (any: any) => void;
+}
+
+export interface IResponseUserData {
+    id: string;
+    avatar_url: string | null;
+    bio: string | undefined;
+    contact: string | undefined;
+    course_module: string;
+    created_at: string;
+    email: string;
+    name: string;
+    techs: ITechs[];
+    updated_at: string;
+    works: IWorks[];
+}
+
+interface ITechs {
+    created_at: string;
+    id: string;
+    status: string;
+    title: string;
+    updated_at: string;
+}
+
+interface IWorks {
+    title: string;
+    description: string;
+    deploy_url: string;
+}
+
+interface IResponseUser {
+    token: string;
+    user: IResponseUserData;
+}
+
+function UserProvider({ children }: IUserProps) {
 
     const navigate = useNavigate()
 
-    const [user, setUser] = useState<string>("")
+    const [user, setUser] = useState<IResponseUserData>({} as IResponseUserData)
 
-    
-    const onSubmit = (data: UserData) => {
+
+
+    const onSubmit = (data: IUserData) => {
         axios
-            .post("https://kenziehub.herokuapp.com/sessions", data)
+            .post<IResponseUser>("https://kenziehub.herokuapp.com/sessions", data)
             .then(response => {
                 setUser(response.data.user)
                 window.localStorage.setItem("@username", response.data.user.name)
@@ -76,9 +115,9 @@ function UserProvider({ children }: UserProps) {
             })
     }
 
-    const createLog = (data: Icadastro) => {
+    const createLog = (data: ICadastro) => {
         axios
-            .post("https://kenziehub.herokuapp.com/users", data)
+            .post<IResponseUser>("https://kenziehub.herokuapp.com/users", data)
             .then(response => {
                 console.log(response)
                 toast.success("Conta criada com sucesso!", {
